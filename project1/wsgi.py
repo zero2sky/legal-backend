825d43543e0a0150c15d12fbed97aba028e15b37
Chetan45s <chetansalmotra45@gmail.com>
2023-06-27 17:08:26 +0530

    deploy

--- project1/wsgi.py
+++ project1/wsgi.py
@@ -21,7 +21,7 @@ import os
 import sys
 
 # assuming your Django settings file is at '/home/myusername/mysite/mysite/settings.py'
-path = '/home/projectnf/Project1'
+path = '/home/zero2sky/legal-backend'
 if path not in sys.path:
     sys.path.insert(0, path)
 
