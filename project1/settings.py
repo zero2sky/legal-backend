33aac5951efb610abde0b9f3f3c9ddb35de332b8
Chetan45s <chetansalmotra45@gmail.com>
2023-06-27 15:07:55 +0530

    test account and otp added

--- project1/settings.py
+++ project1/settings.py
@@ -139,3 +139,11 @@ REST_KNOX = {
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'zerotoskyemail@gmail.com'
+EMAIL_HOST_PASSWORD = 'Email@zero2sky'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
